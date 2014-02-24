4a67eb4a2fd39371bf9e6dd1984c1342c4f01a3a
Jeremy Clemans & Bonnie Bright <student@epicodus.com>
2014-02-24 14:20:37 -0800

    update script file

---
+++ triangle tracker/js/scripts.js
@@ -0,0 +1,36 @@
+var Triangle = {
+  isValid: function() {
+    var result = "";
+      if ((this.side3 >= this.side1 + this.side2) || 
+        (this.side2 >= this.side1 + this.side3) ||
+        (this.side3 >= this.side1 + this.side2)) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  type: function() {
+      if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+        return "Equilateral";
+      } else if ((this.side1 === this.side2) || (this.side1 === this.side3)) { 
+        return "Isosceles";
+      } else {
+        return "Scalene";
+      }
+    }
+};
+
+$(document).ready(function() {
+  $("form#new-triangle").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedSide1 = $("input#side1").val();
+    var inputtedSide2 = $("input#side2").val();
+    var inputtedSide3 = $("input#side3").val();
+    var newTriangle = Object.create(Triangle);
+    newTriangle.side1 = inputtedSide1;
+    newTriangle.side2 = inputtedSide2;
+    newTriangle.side3 = inputtedSide3;
+
+  });
+});
--- triangle tracker/spec/specs.js
+++ triangle tracker/spec/specs.js
@@ -1,6 +1,38 @@
-describe('functionName', function() {
-  it('does something', function() {
-    functionName("input").should.equal("output");
+describe('Triangle', function() {
+  describe('isValid', function() {
+    it("checks to see if three entered lengths are a triangle.", function() {
+      var testValid = Object.create(Triangle);
+      testValid.side1 = 1;
+      testValid.side2 = 2;
+      testValid.side3 = 5;
+      testValid.isValid().should.equal(false);
+    });
+  });
+  describe('type', function() {
+    it("checks user input to see if all three sides are equal.", function() {
+      var testEquilateral = Object.create(Triangle);
+      testEquilateral.side1 = 4;
+      testEquilateral.side2 = 4;
+      testEquilateral.side3 = 4;
+      testEquilateral.type().should.equal("Equilateral");
+    });
+  
+    it("checks input to see if two of three sides are the same length.", function() {
+      var testIsosceles = Object.create(Triangle);
+      testIsosceles.side1 = 5;
+      testIsosceles.side2 = 5;
+      testIsosceles.side3 = 4;
+      testIsosceles.type().should.equal("Isosceles");
+    });
+
+    it("checks input to see if no sides are the same.", function() {
+      var testScalene = Object.create(Triangle);
+      testScalene.side1 = 10;
+      testScalene.side2 = 12;
+      testScalene.side3 = 14;
+      testScalene.type().should.equal("Scalene");
+    });  
+  
   });
 });
 
