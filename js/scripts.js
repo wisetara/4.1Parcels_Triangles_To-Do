9672f1641def0db8cb3bb2826085d814f77b861f
Robin Miller <robin.miller39@gmail.com>
2014-03-04 17:24:58 -0800

    refactored with ids..almost working now

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,50 @@
 var Cell = {
-  initialize: function(Xcoordinate, Ycoordinate) {
-    this.Xcoordinate = Xcoordinate;
-    this.Ycoordinate = Ycoordinate;
-    this.coordinateString = Xcoordinate+","+Ycoordinate;
+  initialize: function(cellId) {
+    this.cellId = cellId;
     this.state = false;
   },
 
-  create: function(Xcoordinate, Ycoordinate) {
+  create: function(cellId) {
     var cell = Object.create(Cell);
-    cell.initialize(Xcoordinate, Ycoordinate);
-    cell.setNeighbors(Xcoordinate, Ycoordinate);
+    cell.initialize(cellId);
+    cell.setNeighbors(cellId);
     return cell;
   },
 
-  setNeighbors: function(X, Y) {
-    if (X === 0 && Y === 0) {
-      this.neighbors = [[X+1, Y],
-                       [X, Y+1], [X+1, Y+1]];
-    } else if (X === 0) {
-      this.neighbors = [[X, Y-1], [X+1, Y-1], [X+1, Y],
-                       [X, Y+1], [X+1, Y+1]];
-    } else if (Y === 0) {
-      this.neighbors = [[X-1, Y], [X+1, Y],
-                      [X-1, Y+1], [X, Y+1], [X+1, Y+1]];
-    } else if (Y === 29 && X === 29) {
-       this.neighbors = [[X, Y-1],
-                       [X-1, Y], [X-1, Y-1]];
-    } else if (X === 29) {
-      this.neighbors = [[X-1, Y-1], [X, Y-1],
-                      [X-1, Y],
-                      [X-1, Y+1], [X, Y+1]];
-    } else if (Y === 29) {
-      this.neighbors = [[X-1, Y-1], [X, Y-1], [X+1, Y-1],
-                      [X-1, Y], [X+1, Y]]
-    }
-    else {
-      this.neighbors = [[X-1, Y-1], [X, Y-1], [X+1, Y-1],
-                      [X-1, Y], [X+1, Y],
-                      [X-1, Y+1], [X, Y+1], [X+1, Y+1]];
-    }
+  setNeighbors: function(cellId) {
+    this.neighbors = [cellId - 31, cellId - 30, cellId - 29,
+                      cellId - 1, cellId + 1,
+                      cellId + 31, cellId + 30, cellId + 29]
   },
 
-  findCellState: function(X, Y) {
-    var searchResult;
-    World.population.forEach(function(currentCell) {
-      if (currentCell.coordinateString === X+","+Y) {
-        searchResult = currentCell;
-      }
-    });
-    return searchResult.state;
-  },
+  // findCellState: function(X, Y) {
+  //   var searchResult;
+  //   World.population.forEach(function(currentCell) {
+  //     if (currentCell.coordinateString === X+","+Y) {
+  //       searchResult = currentCell;
+  //     }
+  //   });
+  //   return searchResult.state;
+  // },
 
-  find: function(X, Y) {
-    var searchResult;
-    World.population.forEach(function(currentCell) {
-      if (currentCell.coordinateString === X+","+Y) {
-        searchResult = currentCell;
-      }
-    });
-    return searchResult;
-  },
+  // find: function(X, Y) {
+  //   var searchResult;
+  //   World.population.forEach(function(currentCell) {
+  //     if (currentCell.coordinateString === X+","+Y) {
+  //       searchResult = currentCell;
+  //     }
+  //   });
+  //   return searchResult;
+  // },
 
   liveNeighbors: function() {
     var counter = 0;
     for (var i = 0; i < this.neighbors.length; i++) {
-      console.log("i = "+i+"neighbors"+this.neighbors);
-      if (Cell.findCellState(this.neighbors[i][0], this.neighbors[i][1]) === undefined) {
+      var currentNeighbor = World.population[this.neighbors[i]];
+      if (currentNeighbor === undefined) {
         i++;
-      } else if (Cell.findCellState(this.neighbors[i][0], this.neighbors[i][1])) {
-        counter += 1;
+      } else if (currentNeighbor.state === true) {
+        counter+=1;
       }
     }
     return counter;
@@ -76,11 +52,11 @@ var Cell = {
 
   setFutureState: function() {
     var numAlive = this.liveNeighbors();
-    if (numAlive < 2) {
+    if (numAlive < 2 && this.state === true) {
       this.futureState = false;
-    } else if (numAlive === 2 || numAlive === 3) {
+    } else if ((numAlive === 2 || numAlive === 3) && this.state === true) {
       this.futureState = true;
-    } else if (numAlive > 3) {
+    } else if (numAlive > 3 && this.state === true) {
       this.futureState = false;
     } else if (this.state === false && numAlive === 3) {
       this.futureState = true;
@@ -88,6 +64,20 @@ var Cell = {
       this.futureState = this.state;
     }
   },
+
+  // if (liveCount < 2 && this.alive === true){
+  //     this.future = false;
+  //   } else if (this.alive === true && liveCount === 2){
+  //     this.future = true;
+  //   } else if (this.alive === true && liveCount === 3){
+  //     this.future = true;
+  //   } else if (liveCount > 3 && this.alive === true){
+  //     this.future = false;
+  //   } else if (this.alive === false && liveCount === 3){
+  //     this.future = true;
+  //   } else if ((this.cellId > 9700 || this.cellId < 301) && this.alive === true){
+  //     this.future = false;
+  //   }
 
   toggleState: function() {
     this.state = !this.state;
@@ -101,46 +91,51 @@ var Cell = {
 
 var World = {
   population: [],
-  populate: function(size) {
-    for(var i = 0; i < size; i++) {
-      for(var j = 0; j < size; j++) {
-        World.population.push(Cell.create(i,j))
-      }
+  populate: function() {
+    for (var i = 0; i < 900; i++) {
+      World.population.push(Cell.create(i));  
     }
   }
-  // iterate: function() {
-  //   World.population.forEach(function(currentCell) {
-  //     currentCell.setFutureState();
-  //     currentCell.makeDeadorAlive();
-  //   });
-  //}
 }
 
 
 $(document).ready(function() {
-  World.populate(30);
-  console.log(World.population[0]);
+  World.populate();
 
   for(var i = 0; i < 30; i++) {
     $("table#world-table").append("<tr></tr>")
     for(var j = 0; j < 30; j++) {
-      $("tr").last().append("<td id ='"+i+","+j+"' class = 'white'></td>")
+      $("tr").last().append("<td id ='" + ((i*30)+j) + "' class = 'white'></td>")
     }
   }
 
   $('table#world-table td').click(function() {
-    Cell.find($(this).parent().index(), $(this).index()).toggleState();
+    var cellId = $(this).attr("id");
+    World.population[cellId].toggleState();
     $(this).toggleClass("black white");
   });
 
   $('button#run-button').click(function() {
-    World.population.forEach(function(cell) {
-      cell.setFutureState();
-      cell.makeDeadorAlive();
-    });
-    // World.iterate();
-    //setInterval(World.iterate(), 500);
+    setInterval(function(){
+      World.population.forEach(function(cell) {
+        cell.setFutureState();
+      });
+
+      World.population.forEach(function(cell) {
+        if (cell.state === true) {
+          $("td#"+cell.cellId).addClass('black');
+          $("td#"+cell.cellId).removeClass('white');
+        } else {
+          $("td#"+cell.cellId).removeClass('black');
+          $("td#"+cell.cellId).addClass('white');
+        }
+      });
+    }, 50);
+
+
   });
+
+
 
 
 
